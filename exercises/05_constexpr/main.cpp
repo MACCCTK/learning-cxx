67c9fe819822ee4c825385cc0875bfe129796317
cyd <wanwwrhdyj@gmail.com>
2026-01-04 22:03:31 +0800

    edit 5

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -1,15 +1,14 @@
 #include "../exercise.h"
 
 constexpr unsigned long long fibonacci(int i) {
-    if (i == 0) return 0;
-    if (i == 1) return 1;
-    unsigned long long a = 0, b = 1;
-    for (int n = 2; n <= i; ++n) {
-        unsigned long long next = a + b;
-        a = b;
-        b = next;
+    switch (i) {
+        case 0:
+            return 0;
+        case 1:
+            return 1;
+        default:
+            return fibonacci(i - 1) + fibonacci(i - 2);
     }
-    return b;
 }
 
 int main(int argc, char **argv) {
@@ -19,7 +18,7 @@ int main(int argc, char **argv) {
 
     // TODO: 观察错误信息，修改一处，使代码编译运行
     // PS: 编译运行，但是不一定能算出结果……
-    constexpr auto ANS_N = 90;
+    constexpr auto ANS_N = 50;
     auto ANS = fibonacci(ANS_N);
     std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
 
